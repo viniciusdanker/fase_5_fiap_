16b295f602abef012ecec3b9d66cfbf80135c7bc
viniciusdanker <63518923+viniciusdanker@users.noreply.github.com>
2020-04-12 10:05:08 -0300

    Delete CategoryRepository.java

--- src/main/java/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import domain.Category;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-
-public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
-          Category findByName(String Name);
-}
